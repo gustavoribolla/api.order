2aea8048b288b09410b0c8167dc68cc5eb060496
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:38:46 -0300

    fix: serialização aos DTOs usados em cache Redis

--- src/main/java/store/order/ItemOut.java
+++ src/main/java/store/order/ItemOut.java
@@ -1,11 +1,14 @@
 package store.order;
 
+import java.io.Serializable;
+
 import lombok.Builder;
 import lombok.experimental.Accessors;
 import store.product.ProductOut;
 
 @Builder @Accessors(fluent = true)
-public record ItemOut(
+public record ItemOut implements Serializable(
+    private static final long serialVersionUID = 1L;
     String id,
     ProductOut product,
     Integer quantity,
--- src/main/java/store/order/OrderOut.java
+++ src/main/java/store/order/OrderOut.java
@@ -1,12 +1,15 @@
 package store.order;
 
+import java.io.Serializable;
+
 import lombok.Builder;
 import lombok.experimental.Accessors;
 import java.util.Date;
 import java.util.List;
 
 @Builder @Accessors(fluent = true)
-public record OrderOut(
+public record OrderOut implements Serializable(
+    private static final long serialVersionUID = 1L;
     String id,
     Date date,
     List<ItemOut> items,
