429cf3eabd449c44de0a0d45d59fa1b65a8b037c
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:45:28 -0300

    fix: definição de record arrumada

--- src/main/java/store/order/ItemOut.java
+++ src/main/java/store/order/ItemOut.java
@@ -7,10 +7,12 @@ import lombok.experimental.Accessors;
 import store.product.ProductOut;
 
 @Builder @Accessors(fluent = true)
-public record ItemOut implements Serializable(
+public record ItemOut(
     private static final long serialVersionUID = 1L;
     String id,
     ProductOut product,
     Integer quantity,
     Double total
-) {}
+) implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/store/order/OrderOut.java
+++ src/main/java/store/order/OrderOut.java
@@ -8,10 +8,12 @@ import java.util.Date;
 import java.util.List;
 
 @Builder @Accessors(fluent = true)
-public record OrderOut implements Serializable(
+public record OrderOut(
     private static final long serialVersionUID = 1L;
     String id,
     Date date,
     List<ItemOut> items,
     Double total
-) {}
+) implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
